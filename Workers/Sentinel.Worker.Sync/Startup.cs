b46038a0ec94c1622ff66770fd5448f25790f30e
Murat MERCAN <mmercan@outlook.com>
2021-08-28 00:24:25 +1000

    common DI added

--- Workers/Sentinel.Worker.Sync/Startup.cs
+++ Workers/Sentinel.Worker.Sync/Startup.cs
@@ -5,17 +5,36 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Sentinel.K8s;
+using k8s;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 namespace Sentinel.Worker.Sync
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            if (Configuration["RunOnCluster"] == "true") { services.AddSingleton<IKubernetesClient>(new KubernetesClient(KubernetesClientConfiguration.InClusterConfig())); }
+            else { services.AddSingleton<IKubernetesClient>(new KubernetesClient(KubernetesClientConfiguration.BuildConfigFromConfigFile())); }
+
+            services.Configure<KestrelServerOptions>(options => { options.AllowSynchronousIO = true; });
+            services.Configure<IISServerOptions>(options => { options.AllowSynchronousIO = true; });
+
+            services.AddSingleton<IServiceCollection>(services);
+            services.AddSingleton<IConfiguration>(Configuration);
+            services.AddAutoMapper(typeof(Startup).Assembly, typeof(Sentinel.K8s.KubernetesClient).Assembly, typeof(Sentinel.Models.CRDs.HealthCheckResource).Assembly);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
