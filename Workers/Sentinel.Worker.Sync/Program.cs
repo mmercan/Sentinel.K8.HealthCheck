f2e6ca7a8280d49e62815182671923310222f818
Murat MERCAN <mmercan@outlook.com>
2021-08-28 00:34:44 +1000

    program class static added

--- Workers/Sentinel.Worker.Sync/Program.cs
+++ Workers/Sentinel.Worker.Sync/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Logging;
 
 namespace Sentinel.Worker.Sync
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
