073eacdc6e2038b03f20e592dcc14cc77ea65086
Murat MERCAN <mmercan@outlook.com>
2021-09-10 14:12:58 +1000

    find event tests added

--- Libs/Sentinel.K8s.Tests/K8sEventOpsTests.cs
+++ Libs/Sentinel.K8s.Tests/K8sEventOpsTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Sentinel.K8s.Tests.Helpers;
 using Sentinel.K8s.Watchers;
@@ -16,10 +17,11 @@ namespace Sentinel.K8s.Tests
 
 
         [Theory]
-        [InlineData("my-new-healthcheck-object-2", "default", "message_2")]
+        [InlineData("my-new-healthcheck-object-2", "default", "message_")]
         public async Task K8sEventOpsShuldHaveAnInstance(string name, string @namespace, string message)
         {
 
+            var message_1 = message + DateTime.Now.ToString();
             var k8Client = KubernetesClientTestHelper.GetKubernetesClient();
             var healthobj = await k8Client.Get<HealthCheckResource>(name: name, @namespace: @namespace);
 
@@ -27,12 +29,41 @@ namespace Sentinel.K8s.Tests
 
             K8sEventOps ops = KubernetesClientTestHelper.GetK8sEventOps();
 
+
+            //Create new Event
             var eve = await ops.CountUpOrCreateEvent<HealthCheckResource>(@namespace, healthobj, message);
 
-            _output.WriteLine("event message : " + eve.Message);
-            Assert.NotNull(eve.Metadata.Name);
+
+            //Count up 
+            var eve_2 = await ops.CountUpOrCreateEvent<HealthCheckResource>(@namespace, healthobj, message);
+            _output.WriteLine("event message : " + eve_2.Message);
+            Assert.NotNull(eve_2.Metadata.Name);
+
+        }
 
 
+        [Theory]
+        [InlineData("default")]
+        public async Task K8sEventOpsListEventsInNamespace(string @namespace)
+        {
+            K8sEventOps ops = KubernetesClientTestHelper.GetK8sEventOps();
+
+            var eventList = await ops.GetAsync(@namespace);
+
+            _output.WriteLine("event count : " + eventList.Count);
+            Assert.NotNull(eventList);
+
+        }
+
+        [Fact]
+        public async Task K8sEventOpsListEventsInAllNamespaces()
+        {
+            K8sEventOps ops = KubernetesClientTestHelper.GetK8sEventOps();
+
+            var eventList = await ops.GetAllAsync();
+
+            _output.WriteLine("event count : " + eventList.Count);
+            Assert.NotNull(eventList);
 
         }
     }
