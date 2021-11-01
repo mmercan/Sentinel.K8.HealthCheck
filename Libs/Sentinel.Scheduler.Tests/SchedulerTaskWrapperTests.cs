a1a47691046126a46a0620ff6e3f2fc53a973118
Murat MERCAN <mmercan@outlook.com>
2021-11-01 12:45:14 +1100

    test updated

--- Libs/Sentinel.Scheduler.Tests/SchedulerTaskWrapperTests.cs
+++ Libs/Sentinel.Scheduler.Tests/SchedulerTaskWrapperTests.cs
@@ -1,6 +1,8 @@
+using System;
 using Sentinel.Models.K8sDTOs;
 using Sentinel.Scheduler.GeneralScheduler;
 using Sentinel.Scheduler.GeneralScheduler.Cron;
+using TimeZoneConverter;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -19,10 +21,11 @@ namespace Sentinel.Scheduler.Tests
         [Fact]
         public void SchedulerTaskWrapperShouldCreateaNewInstance()
         {
+            TimeZoneInfo tzi = TZConvert.GetTimeZoneInfo("Australia/Melbourne");
             SchedulerTaskWrapper<HealthCheckResourceV1> wrapper = new GeneralScheduler.SchedulerTaskWrapper<HealthCheckResourceV1>();
             wrapper.Schedule = CrontabSchedule.Parse("*/3 * * * *");
             wrapper.Increment();
-            wrapper.ShouldRun(System.DateTime.Now);
+            wrapper.ShouldRun(System.DateTime.Now, tzi);
             //CrontabSchedule schedule 
             //    var dt = new DateTime(2021, 12, 31, 23, 58, 0);
             //    var nextOccurrences = schedule.GetNextOccurrences(dt, dt.AddDays(-1));
