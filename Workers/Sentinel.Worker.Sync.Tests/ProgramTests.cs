bac4e0645be5d1796b4af62fffd3dfeaabce475d
Murat MERCAN <mmercan@outlook.com>
2021-11-16 23:22:24 +1100

    Program updated

--- Workers/Sentinel.Worker.Sync.Tests/ProgramTests.cs
+++ Workers/Sentinel.Worker.Sync.Tests/ProgramTests.cs
@@ -16,9 +16,9 @@ namespace Sentinel.Worker.Sync.Tests
             Task.Run(cancellationToken: source.Token, action: () =>
              {
                  var args = new string[1] { "" };
-                 // Program.Main(args);
+                 Program.Main(args);
 
-                 var pr = new Program();
+                 //var pr = new Program();
 
              });
         }
