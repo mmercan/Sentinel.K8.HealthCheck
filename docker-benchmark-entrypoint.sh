5122ac0e4c843f5f263d83ae57edaab19483a2a7
Murat MERCAN <mmercan@outlook.com>
2021-11-23 19:56:23 +1100

    output folder updated

--- docker-benchmark-entrypoint.sh
+++ docker-benchmark-entrypoint.sh
@@ -2,7 +2,7 @@
 
 dotnet build ./Sentinel.K8.HealthCheck.sln
 
-dotnet build ./Libs/Sentinel.Scheduler.Benchmark -c Release --output ./output/Sentinel.Scheduler.Benchmark
+dotnet build ./Libs/Sentinel.Scheduler.Benchmark -c Release --output /output/Sentinel.Scheduler.Benchmark
 echo "Console output"
 
-dotnet ./output/Sentinel.Scheduler.Benchmark/Sentinel.Scheduler.Benchmark.dll
+dotnet /output/Sentinel.Scheduler.Benchmark/Sentinel.Scheduler.Benchmark.dll
