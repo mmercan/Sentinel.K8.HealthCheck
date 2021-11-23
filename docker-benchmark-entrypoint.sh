e840aee921aa527e99da7e2b62c347ecf9140859
Murat MERCAN <mmercan@outlook.com>
2021-11-23 21:26:48 +1100

    mkdir added

--- docker-benchmark-entrypoint.sh
+++ docker-benchmark-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dotnet build ./Sentinel.K8.HealthCheck.sln
+# dotnet build ./Sentinel.K8.HealthCheck.sln
 
 dotnet build ./Libs/Sentinel.Scheduler.Benchmark -c Release --output /output/Sentinel.Scheduler.Benchmark
 echo "Console output"
@@ -8,5 +8,5 @@ echo "Console output"
 dotnet /output/Sentinel.Scheduler.Benchmark/Sentinel.Scheduler.Benchmark.dll --artifacts /Benchmarks/Sentinel.Scheduler --filter *
 
 
-
+mkdir -p /Benchmarks/Sentinel.Scheduler
 cp -r ./BenchmarkDotNet.Artifacts/results /Benchmarks/Sentinel.Scheduler
