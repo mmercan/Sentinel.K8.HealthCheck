81e48385a64ebfed80d3456cb49c3eafb0ab16dd
Murat MERCAN <mmercan@outlook.com>
2021-11-23 21:21:54 +1100

    BenchmarkRunner.Run added back and second Benchmarkclass added

--- Libs/Sentinel.Scheduler.Benchmark/Program.cs
+++ Libs/Sentinel.Scheduler.Benchmark/Program.cs
@@ -9,7 +9,9 @@ namespace Sentinel.Scheduler.Benchmark
         static void Main(string[] args)
         {
             BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
-            // BenchmarkRunner.Run<SchedulerRepositoryBenchmark>();
+
+            BenchmarkRunner.Run<SchedulerRepositoryBenchmark>();
+            BenchmarkRunner.Run<SchedulerRepositoryBenchmarkSec>();
         }
 
     }
---
+++ Libs/Sentinel.Scheduler.Benchmark/SchedulerRepositoryBenchmarkSec.cs
@@ -0,0 +1,37 @@
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using Sentinel.Models.K8sDTOs;
+
+namespace Sentinel.Scheduler.Benchmark
+{
+    [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class SchedulerRepositoryBenchmarkSec
+    {
+
+
+
+        [Benchmark]
+
+        public void GetSchedulerRepositorySec()
+        {
+
+            var logger = Sentinel.Tests.Helpers.Helpers.GetLogger<SchedulerRepository<HealthCheckResourceV1>>();
+            var repo = new SchedulerRepository<HealthCheckResourceV1>(logger);
+
+            var hc = new HealthCheckResourceV1();
+            hc.Schedule = "* * * * *";
+            hc.Name = "test";
+            hc.Namespace = "default";
+
+            repo.Items.Add(hc);
+
+            repo.UpdateItem(hc);
+
+            repo.Items.Remove(hc);
+
+
+        }
+    }
+}
