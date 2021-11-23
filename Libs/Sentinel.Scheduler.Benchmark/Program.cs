f85182623cf8e87c4061b01234bfb8df737e26fa
Murat MERCAN <mmercan@outlook.com>
2021-11-23 14:22:22 +1100

    Benchmark added

---
+++ Libs/Sentinel.Scheduler.Benchmark/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace Sentinel.Scheduler.Benchmark
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<SchedulerRepositoryBenchmark>();
+        }
+
+    }
+}
