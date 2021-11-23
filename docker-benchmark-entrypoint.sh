8688f0df64ba63db7f329d4f183e87e9d1674d8f
Murat MERCAN <mmercan@outlook.com>
2021-11-23 20:51:56 +1100

    filter added

--- docker-benchmark-entrypoint.sh
+++ docker-benchmark-entrypoint.sh
@@ -5,4 +5,4 @@ dotnet build ./Sentinel.K8.HealthCheck.sln
 dotnet build ./Libs/Sentinel.Scheduler.Benchmark -c Release --output /output/Sentinel.Scheduler.Benchmark
 echo "Console output"
 
-dotnet /output/Sentinel.Scheduler.Benchmark/Sentinel.Scheduler.Benchmark.dll --artifacts /Benchmarks/Sentinel.Scheduler
+dotnet /output/Sentinel.Scheduler.Benchmark/Sentinel.Scheduler.Benchmark.dll --artifacts /Benchmarks/Sentinel.Scheduler --filter *
